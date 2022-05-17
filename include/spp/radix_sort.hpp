c4b8e3ed7cdc2c939ba3af9c5b50af49d18f2e4d
jl.lu@ubuntu.laptop <jllucs@zju.edu.cn>
2022-05-17 13:28:35 +0800

    [opt] u16 counters inside warp
    - u16 is sufficient for recording thread- and warp-level perfixes
    - The register usage for each thread is 64 now, for Pascal arch.
    - Performance is about 2 / 3 of cub's.

--- include/spp/radix_sort.hpp
+++ include/spp/radix_sort.hpp
@@ -328,20 +328,26 @@ namespace spp {
 
 
 		u32 thread_keys[ItemsPerThread];
-		usize thread_exclusive_prefixes[ItemsPerThread];
+		u16 thread_exclusive_prefixes[ItemsPerThread];
 
-		__shared__ Histogram<u32> shared_warp_exclusive_prefixes[WarpsPerBlock];
+		__shared__ Histogram<u16> shared_warp_exclusive_prefixes[WarpsPerBlock];
 		__shared__ Histogram<u32> shared_block_exclusive_prefixes;
 		__shared__ Histogram<u32> shared_grid_exclusive_prefix;
+
+		Histogram<u16> & this_warp_exclusive_prefix = shared_warp_exclusive_prefixes[warp.meta_group_rank()];
 
 		if (block.thread_rank() < HistogramSize) {
 			usize const radix_bank = block.thread_rank();
 			for (usize i_warp = 0; i_warp < WarpsPerBlock; ++i_warp) {
-				shared_warp_exclusive_prefixes[i_warp][radix_bank] = 0_u32;
+				shared_warp_exclusive_prefixes[i_warp][radix_bank] = 0_u16;
 			}
 			shared_block_exclusive_prefixes[radix_bank] = 0_u32;
 			shared_grid_exclusive_prefix[radix_bank] = (*p_grid_radix_histogram)[radix_bank];
 		}
+
+		// __shared__ Histogram<u32> shared_warp_ballot_buffers[WarpsPerBlock];
+
+		// Histogram<u32> & this_warp_ballot_buffer = shared_warp_ballot_buffers[warp.meta_group_rank()];
 
 		block.sync();
 
@@ -351,22 +357,21 @@ namespace spp {
 			usize const in_rank = block_rank_begin + warp_rank_begin + 32 * i_key + warp.thread_rank();
 			bool const is_active = in_rank < num_keys;
 
-			auto const key = is_active ? keys_in[in_rank] : 0_u32;
-			auto const radix_bank = binning(key);
+			u32 const key = is_active ? keys_in[in_rank] : 0_u32;
+			usize const radix_bank = binning(key);
 
-			auto const active_mask = warp.ballot(is_active);
-			auto const match_mask = match_any(active_mask, radix_bank);
-			auto const match_count = __popc(match_mask);
-			auto const match_exclusive_prefix = __popc(match_mask & thread_mask_lt(warp));
+			u32 const match_mask = match_any(warp, is_active, radix_bank);
+			u32 const match_count = __popc(match_mask);
+			u32 const match_exclusive_prefix = __popc(match_mask & thread_mask_lt(warp));
 			
-			auto const thread_exclusive_prefix = shared_warp_exclusive_prefixes[warp.meta_group_rank()][radix_bank] + match_exclusive_prefix;
+			u16 const thread_exclusive_prefix = this_warp_exclusive_prefix[radix_bank] + u16(match_exclusive_prefix);
 			warp.sync();
 
 			thread_keys[i_key] = key;
 			thread_exclusive_prefixes[i_key] = thread_exclusive_prefix;
 
 			if (match_exclusive_prefix + 1 == match_count) {
-				shared_warp_exclusive_prefixes[warp.meta_group_rank()][radix_bank] += match_count;
+				this_warp_exclusive_prefix[radix_bank] += match_count;
 			}
 			warp.sync();
 		}
@@ -381,7 +386,7 @@ namespace spp {
 			auto block_reduction = 0_u32;
 
 			for (usize i_warp = 0; i_warp < WarpsPerBlock; ++i_warp) {
-				u32 const warp_reduction = shared_warp_exclusive_prefixes[i_warp][radix_bank];
+				u16 const warp_reduction = shared_warp_exclusive_prefixes[i_warp][radix_bank];
 				shared_warp_exclusive_prefixes[i_warp][radix_bank] = block_reduction;
 				block_reduction += warp_reduction;
 			}
@@ -415,184 +420,14 @@ namespace spp {
 
 				usize const out_rank = shared_grid_exclusive_prefix[radix_bank]
 					+ shared_block_exclusive_prefixes[radix_bank]
-					+ shared_warp_exclusive_prefixes[warp.meta_group_rank()][radix_bank]
-					+ thread_exclusive_prefixes[i_key];
+					+ this_warp_exclusive_prefix[radix_bank]
+					+ usize(thread_exclusive_prefixes[i_key]);
 					
 				keys_out[out_rank] = thread_keys[i_key];
-
-				// if (out_rank < 16) {
-				// 	printf("i_key = %u, in rank = %u, out rank = %u, value = %x, (%u, %u, %u, %x) from thread %u\n",
-				// 		i_key,
-				// 		in_rank,
-				// 		out_rank,
-				// 		thread_keys[i_key],
-				// 		u32(shared_grid_exclusive_prefix[radix_bank]),
-				// 		u32(shared_block_exclusive_prefixes[radix_bank]),
-				// 		u32(shared_warp_exclusive_prefixes[warp.meta_group_rank()][radix_bank]),
-				// 		u32(thread_exclusive_prefixes[i_key]),
-				// 		u32(grid.thread_rank())
-				// 	);
-				// }
 			}
 		}
 
 	}
-
-
-
-	// template <usize ItemsPerThread, usize ThreadsPerBlock, usize RadixLaneBits, typename Histogram, typename Binning, typename MatchAny>
-	// __global__
-	// void radix_scan_by_key(u32 const * keys_in, u32 * keys_out, usize num_keys,
-	// 	block_descriptor<Histogram> volatile * block_descriptors,
-	// 	Histogram const * p_radix_bank_histogram,
-	// 	Binning binning, MatchAny match_any) {
-
-	// 	usize constexpr WarpsPerBlock	= ThreadsPerBlock / 32;
-	// 	usize constexpr ItemsPerWarp	= ItemsPerThread * 32;
-	// 	usize constexpr ItemsPerBlock	= ItemsPerThread * ThreadsPerBlock;
-
-
-
-	// 	auto const grid					= cg::this_grid();
-	// 	auto const block				= cg::this_thread_block();
-	// 	auto const warp					= cg::tiled_partition<32>(block);
-
-	// 	usize const block_rank_begin	= grid.block_rank() * ItemsPerBlock;
-	// 	usize const warp_rank_begin		= warp.meta_group_rank() * ItemsPerWarp;
-		
-
-
-	// 	u32 thread_keys[ItemsPerThread];
-	// 	usize thread_key_exclusive_prefixes[ItemsPerThread];
-
-	// 	__shared__ Histogram warp_exclusive_prefixes[WarpsPerBlock];
-	// 	__shared__ Histogram block_exclusive_prefix;
-
-	// 	static_assert(block_exclusive_prefix.size() % 32 == 0, "The width of histogram is not a multiple of 32.");
-
-	// 	for (usize i = 0; i < block_exclusive_prefix.size() / 32; ++i) {
-	// 		usize const radix_bank = i * 32 + warp.meta_group_rank();
-	// 		warp_exclusive_prefixes[warp.meta_group_rank()][radix_bank] = 0;
-	// 	}
-
-	// 	__shared__ Histogram radix_bank_histogram;
-
-	// 	for (usize radix_bank = block.thread_rank(); radix_bank < radix_bank_histogram.size(); radix_bank += block.num_threads()) {
-	// 		radix_bank_histogram[block.thread_rank()] = (*p_radix_bank_histogram)[block.thread_rank()];
-	// 	}
-
-	// 	block.sync();
-
-
-
-	// 	barrier<1, WarpsPerBlock>		warp_reduction_ready;
-	// 	barrier<2, WarpsPerBlock - 1>	warp_prefix_ready;
-	// 	barrier<3, WarpsPerBlock>		block_prefix_ready;
-		
-
-
-	// 	for (usize i_key = 0; i_key < ItemsPerThread; ++i_key) {
-	// 		usize const in_rank = block_rank_begin + warp_rank_begin + 32 * i_key + warp.thread_rank();
-			
-	// 		if (in_rank < num_keys) {
-	// 			auto const key = keys_in[in_rank];
-
-	// 			auto const active_mask = __activemask();
-	// 			auto const match_mask = match_any(active_mask, key);
-	// 			auto const match_count = __popc(match_mask);
-	// 			auto const match_exclusive_prefix = __popc(match_mask & thread_mask_lt(warp));
-
-	// 			auto const key_bank = binning(key);
-	// 			if (match_exclusive_prefix + 1 == match_count) {
-	// 				warp_exclusive_prefixes[key_bank] += match_count;
-	// 			}
-
-	// 			thread_keys[i_key] = key;
-	// 			thread_key_exclusive_prefixes[i_key] = match_exclusive_prefix;
-	// 		}
-	// 		else {
-	// 			thread_keys[i_key] = 0;
-	// 			thread_key_exclusive_prefixes[i_key] = ~ 0_us;
-	// 		}
-	// 	}
-
-
-
-	// 	if (warp.meta_group_rank() == 0) {
-	// 		warp_reduction_ready.arrive();
-
-	// 		auto add_histogram = [&] (Histogram & a, Histogram const & b) {
-	// 			for (usize i = 0; i < a.size() / 32; ++i) {
-	// 				usize const radix_bank = i * 32 + warp.thread_rank();
-	// 				a[radix_bank] += b[radix_bank];
-	// 			}
-	// 		};
-
-	// 		for (auto i_block = isize(grid.block_rank()) - 1; 0 <= i_block; --i_block) {
-	// 			block_status const status = block_descriptors[i_block].load_status_spinning();
-				
-	// 			if (status == block_status::aggregated) {
-	// 				add_histogram(block_exclusive_prefix, block_descriptors[i_block].internal_aggregate);
-	// 			}
-	// 			else {
-	// 				add_histogram(block_exclusive_prefix, block_descriptors[i_block].inclusive_prefix);
-	// 				break;
-	// 			}
-	// 		}
-
-	// 		block_prefix_ready.arrive();
-	// 	}
-	// 	else if (warp.meta_group_rank() + 1 == WarpsPerBlock) {
-	// 		warp_reduction_ready.wait();
-
-	// 		auto radix_vector = vector<u16, 32>::bind(warp);
-			
-	// 		auto const subwarp = cg::tiled_partition<RadixBankCount>(warp);
-
-	// 		if (subwarp.meta_group_rank() == 0) {
-	// 			for (usize i_warp = 0; i_warp < WarpsPerBlock; ++i_warp) {
-	// 				auto const prefix = warp_bank_exclusive_prefixes[i_warp].bind(subwarp);
-	// 				warp_bank_exclusive_prefixes[i_warp].bind(subwarp) += radix_vector;
-	// 				radix_vector += prefix;
-	// 			}
-	// 		}
-
-	// 		warp_prefix_ready.arrive();
-
-	// 		if (subwarp.meta_group_rank() == 0) {
-	// 			block_descriptors[grid.block_rank()].internal_aggregate.bind(subwarp) = radix_vector;
-	// 			__threadfence();
-	// 			block_descriptors[grid.block_rank()].status = block_status::aggregated;
-	// 		}
-
-	// 		block_prefix_ready.wait();
-
-	// 		if (subwarp.meta_group_rank() == 0) {
-	// 			block_descriptors[grid.block_rank()].internal_aggregate.bind(subwarp) = block_exclusive_prefix.bind(subwarp) + radix_vector;
-	// 			__threadfence();
-	// 			block_descriptors[grid.block_rank()].status = block_status::prefixed;
-	// 		}
-	// 	}
-	// 	else {
-	// 		warp_reduction_ready.arrive();
-
-	// 		warp_prefix_ready.wait();
-
-	// 		block_prefix_ready.wait();
-	// 	}
-
-
-
-	// 	for (usize i_key = 0; i_key < ItemsPerThread; ++i_key) {
-	// 		usize const radix_bank = binning(thread_keys[i_key]);
-	// 		usize const in_rank = block_rank_begin + warp_rank_begin + 32 * i_key + warp.thread_rank();
-	// 		usize const out_rank = radix_bank_histogram[radix_bank] + block_exclusive_prefix[radix_bank] + warp_exclusive_prefixes[warp.meta_group_rank()][radix_bank] + thread_exclusive_prefixes[i_key];
-
-	// 		if (in_rank < num_keys) keys_out[out_rank] = thread_keys[i_key];
-	// 		else break;
-	// 	}
-
-	// }
 
 
 
@@ -617,14 +452,26 @@ namespace spp {
 
 		struct match_any {
 			__device__
-			u32 operator()(u32 mask, u32 value) const noexcept {
-				u32 match = mask;
+			u32 operator()(cg::thread_block_tile<32> const & warp, bool is_thread_active, u32 value) const noexcept {
+				// for (usize i = 0; i < ballot_buffer.size / 32; ++i) {
+				// 	ballot_buffer[i * 32 + warp.thread_rank()] = 0_u32;
+				// }
+				// warp.sync();
+
+				// if (is_thread_active) {
+				// 	atomicOr(ballot_buffer.addr_at(value), 1_u32 << warp.thread_rank());
+				// }
+				// warp.sync();
+
+				// return ballot_buffer[value];
+
+				u32 mask = warp.ballot(is_thread_active);
 				for (usize i = 0; i < radix_lane_bits; ++i) {
 					bool const bit = (value >> i) & 1_u32;
-					u32 const ballot = __ballot_sync(mask, bit);
-					match &= bit ? ballot : (~ ballot);
+					u32 const ballot = warp.ballot(bit);
+					mask &= bit ? ballot : (~ ballot);
 				}
-				return match;
+				return mask;
 			}
 		};
 
